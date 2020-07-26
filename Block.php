2713febea5f9f9e98d441e842d7b183a28ac7fd5
BernhardBaumrock <office@baumrock.com>
2020-07-26 16:39:02 +0200

    move hooks to block

--- Block.php
+++ Block.php
@@ -14,42 +14,6 @@ abstract class Block extends \ProcessWire\Page {
     ]);
   }
 
-  public function init() {
-
-    /**
-     * Add hook that triggers the saveReady() method of the block
-     */
-    $this->addHookAfter("Pages::saveReady", function(HookEvent $event) {
-      $page = $event->arguments(0);
-      if(!$page instanceof Block) return;
-      $this->saveReady($event);
-    });
-
-    /**
-     * Add hook on buildform that calls the buildForm() method of the
-     * edited block.
-     */
-    $this->addHookAfter("ProcessPageEdit::buildForm", function(HookEvent $event) {
-      $page = $event->object->getPage();
-      if(!$page instanceof Block) return;
-      $this->buildForm($event);
-    });
-  }
-
-  /**
-   * This hook is called for every block and by default does nothing
-   * Just add your own in the specific block instance!
-   * @return void
-   */
-  public function buildForm(HookEvent $event) {}
-
-  /**
-   * This hook is called for every block and by default does nothing
-   * Just add your own in the specific block instance!
-   * @return void
-   */
-  public function saveReady(HookEvent $event) {}
-
   /**
    * Block Migrations
    */
--- blocks/Headline.php
+++ blocks/Headline.php
@@ -1,4 +1,5 @@
 <?php namespace RockMatrixBlock;
+use ProcessWire\HookEvent;
 class Headline extends \RockMatrix\Block {
 
   const tpl = self::prefix."headline"; // rmblock_headline
@@ -10,6 +11,34 @@ class Headline extends \RockMatrix\Block {
       'title' => $this->_('Headline'),
       'description' => $this->_('Text will be rendered as Headline.'),
     ]);
+  }
+
+  public function init() {
+    $this->addHookAfter("Pages::saveReady", $this, "saveReady");
+    $this->addHookAfter("ProcessPageEdit::buildForm", $this, "buildForm");
+  }
+
+  public function saveReady(HookEvent $event) {
+    $page = $event->arguments(0);
+    if($page->template != self::tpl) return;
+
+    if(!$page->id) {
+      $page->name = $event->pages->names()->uniqueRandomPageName(5);
+      $page->title = $page->title ?: 'Headline';
+    }
+  }
+
+  /**
+   * Build the edit form for this block
+   */
+  public function buildForm(HookEvent $event) {
+    $form = $event->arguments(0);
+    $page = $event->object->getPage();
+    if($page->template != self::tpl) return;
+
+    if($f = $form->get('title')) {
+      $f->label = $this->_('Headline');
+    }
   }
 
   public function migrate() {

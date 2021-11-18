34964a17366e1100561603935b7ac1701c499e5f
BernhardBaumrock <office@baumrock.com>
2021-11-18 15:13:33 +0100

    tweak image block

--- blocks/Image.php
+++ blocks/Image.php
@@ -66,7 +66,13 @@ class Image extends \RockMatrix\Block {
   public function render() {
     $image = $this->get(self::field_image);
     if(!$image) return;
-    return "<div class='rmblock-image'><img src='{$image->url}'></div>";
+    $label = $image->description;
+    $img = "<img data-src='{$image->maxSize(200,200)->url}' uk-img>";
+    $img = "<a href='{$image->maxSize(1600,1600)->url}'>$img</a>";
+    return "<div class='rmblock-image' uk-lightbox>
+      $img
+      <div class='label uk-text-small'>$label</div>
+      </div>";
   }
 
 }

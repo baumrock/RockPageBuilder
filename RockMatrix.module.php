179924eccf3d94f54c23a53da9fdafd2da7efa60
BernhardBaumrock <office@baumrock.com>
2022-07-28 12:56:52 +0200

    bump version

--- RockMatrix.module.php
+++ RockMatrix.module.php
@@ -40,7 +40,7 @@ class RockMatrix extends WireData implements Module, ConfigurableModule {
   public static function getModuleInfo() {
     return [
       'title' => 'RockMatrix',
-      'version' => '2.2.7',
+      'version' => '2.2.8',
       'summary' => 'Master module for RockMatrix Fieldtype + Inputfield',
       'autoload' => 90, // RockFields has 100 and loads earlier
       'singular' => true,

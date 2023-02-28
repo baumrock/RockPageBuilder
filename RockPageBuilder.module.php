5aa340018275de1846317cc29f9e96756388271d
BernhardBaumrock <office@baumrock.com>
2023-02-28 11:26:52 +0100

    bump version

--- RockPageBuilder.module.php
+++ RockPageBuilder.module.php
@@ -58,7 +58,7 @@ class RockPageBuilder extends WireData implements Module, ConfigurableModule
   {
     return [
       'title' => 'RockPageBuilder',
-      'version' => '3.19.0',
+      'version' => '3.20.0',
       'summary' => 'Master module for RockPageBuilder Fieldtype + Inputfield',
       'autoload' => 90, // RockFields has 100 and loads earlier
       'singular' => true,

bdca035523950a3e5a01ac50d55b0b19e2ac6c04
BernhardBaumrock <office@baumrock.com>
2023-04-29 20:05:35 +0200

    feat: add showAll property You can set this property on a field, then all items will be shown by default.

--- Block.php
+++ Block.php
@@ -206,7 +206,7 @@ class Block extends \ProcessWire\Page
     /** @var RockFields $rf */
     if (!$rf = $this->wire->rockfields) {
       $f->notes .= "\nYou also need to install the RockFields module to use this feature.";
-      $fs->add($f);
+      if ($this->wire->user->isSuperuser()) $fs->add($f);
       return;
     }
     if (!$f = $rf->getInputfield($this, $this->settingsName(), true)) {
@@ -584,9 +584,6 @@ class Block extends \ProcessWire\Page
     if ($f = $fs->get('title')) {
       if ($this->getInfo()->hideTitle) {
         $f->collapsed = Inputfield::collapsedHidden;
-        if ($fs->children()->count() === 1) {
-          $fs->notes = 'This block has no fields';
-        }
       }
     }
     $this->buildForm($fs);

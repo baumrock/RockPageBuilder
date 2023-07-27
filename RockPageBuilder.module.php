f33da3d53250aa90a1edaadf47fb4e282bbb5b5a
BernhardBaumrock <office@baumrock.com>
2023-07-27 14:34:34 +0200

    add module info bullets

--- RockPageBuilder.module.php
+++ RockPageBuilder.module.php
@@ -1612,8 +1612,21 @@ class RockPageBuilder extends WireData implements Module, ConfigurableModule
    */
   public function getModuleConfigInputfields($inputfields)
   {
-    $data = $this->data;
+    $name = strtolower($this);
+    $inputfields->add([
+      'type' => 'markup',
+      'label' => 'Documentation & Updates',
+      'icon' => 'life-ring',
+      'value' => "<p>Hey there, coding rockstars! 👋</p>
+        <ul>
+          <li><a class=uk-text-bold href=https://www.baumrock.com/modules/$name/docs>Read the docs</a> and level up your coding game! 🚀💻😎</li>
+          <li><a class=uk-text-bold href=https://www.baumrock.com/rock-monthly>Sign up now for our monthly newsletter</a> and receive the latest updates and exclusive offers right to your inbox! 🚀💻📫</li>
+          <li><a class=uk-text-bold href=https://github.com/baumrock/$name>Show some love by starring the project</a> and keep me motivated to build more awesome stuff for you! 🌟💻😊</li>
+          <li><a class=uk-text-bold href=https://paypal.me/baumrockcom>Support my work with a donation</a>, and together, we'll keep rocking the coding world! 💖💻💰</li>
+        </ul>",
+    ]);
 
+    $data = $this->data;
     $inputfields->add([
       'type' => 'markup',
       'label' => 'Note',

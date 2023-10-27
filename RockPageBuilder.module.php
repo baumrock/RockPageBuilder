3944e10caaf6d351e9526245b68b2224d8d495d9
BernhardBaumrock <office@baumrock.com>
2023-10-27 18:57:56 +0200

    fix: update breaking block styles

--- RockPageBuilder.module.php
+++ RockPageBuilder.module.php
@@ -518,8 +518,8 @@ class RockPageBuilder extends WireData implements Module, ConfigurableModule
     if (!strpos($html, "#rpbstyle-")) return;
     foreach ($this->blockStylesCache as $id => $str) {
       // markup can either be in quotes (latte) or without quotes (php)
-      $html = str_replace("$id", $str, $html);
       $html = str_replace("\"$id\"", $str, $html);
+      $html = str_replace("$id", $str, $html);
     }
     $event->return = $html;
   }

42650cfdc54087d184e2523d591c3277e02b2686
BernhardBaumrock <office@baumrock.com>
2025-02-11 13:18:09 +0100

    feat: add fallback to default field in renderBlocks()

--- RockPageBuilder.module.php
+++ RockPageBuilder.module.php
@@ -1600,10 +1600,11 @@ class RockPageBuilder extends WireData implements Module, ConfigurableModule
 
   public function renderBlocks(
     Page|string|int $page,
-    string $field,
+    string $field = null,
     bool $renderPlus = true,
   ) {
     $p = wire()->pages->get((string)$page);
+    if (!$field) $field = self::field_blocks;
     $field = $p->getFormatted($field);
     if (!$field) return;
     return $field->render($renderPlus);
